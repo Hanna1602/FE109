6cd54513985b3e69dc9fd1ef8ef7a8f74b119457
VadimM <vadim.mikutsevich@gmail.com>
2021-04-29 16:00:39 +0300

    vadim m

--- js/js-task_2/index.js
+++ js/js-task_2/index.js
@@ -1,1 +1,64 @@
 
+
+// Задание 1
+
+let name = prompt('Ваше имя');
+let age = prompt('Возраст');
+let city = prompt('Город проживания');
+let phone = prompt('Телефон');
+let email = prompt('Почта');
+let company = prompt('Место работы');
+
+alert(`Меня зовут ${name}. Мне ${age} лет.
+ 	Я проживаю в городе ${city} и работаю в компнаии ${company}.
+ 	Мои контактнеы данные:
+	Почта ${email} и телефон ${phone}`);
+
+// Задание 2
+
+let birth = 2021 - age;
+alert(`${name} родился ${birth}`);
+
+//Задание 3
+
+let num = '123456';
+
+if ((num[1] + num[2] + num[3]) === (num[4] + num[5] + num[6])) {
+	console.log('Верно!')
+}
+else {
+	console.log('Неверно')
+};
+
+//Задание 4
+
+let a = 1
+if (a > 0) {
+	console.log('Верно')
+}
+else {
+	console.log('Неверно')
+}
+
+// Задание 5
+let a = 10;
+let b = 2;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+let sum = a + b
+if (sum > 1) {
+	let result = Math.pow(sum, 2)
+	console.log(result)
+};
+
+// Задание 6
+if ((a > 2 && a < 11) || (b >= 6 && b < 14)) {
+	console.log('Верно')
+}
+else {
+	console.log('Неверно')
+}
+
+
--- js/js-task_2/kata.js
+++ js/js-task_2/kata.js
@@ -1,11 +1,21 @@
-function basicOp(operation, value1, value2){
-    
+function basicOp(operation, value1, value2) {
+	if (operation == '+') return value1 + value2
+	if (operation == '-') return value1 - value2
+	if (operation == '*') return value1 * value2
+	if (operation == '/') return value1 / value2
 }
 
 function bonusTime(salary, bonus) {
-    
+	if (bonus == true) { return "\u00A3" + salary * 10 }
+	else (bonus == false)
+	{ return "\u00A3" + salary }
 }
 
-function getGrade (s1, s2, s3) {
-   
+function getGrade(s1, s2, s3) {
+  let x = (s1 + s2 + s3) / 3
+  if (x >= 90) return 'A'
+  else if (x >= 80 ) return 'B'
+  else if (x >= 70 ) return 'C'
+  else if (x >= 60 ) return 'D'
+  else if (x >= 0 ) return 'F'
 }
