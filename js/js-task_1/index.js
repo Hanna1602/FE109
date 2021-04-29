d57a418e7ca9544cbff4fd967390f6b57d1d90e8
Дон безопасник <akozhapenko@mail.ru>
2021-04-29 21:40:00 +0300

    js1

---
+++ js/js-task_1/index.js
@@ -0,0 +1,95 @@
+ // 1
+
+ let str = 'Привет',
+      sum = 123,
+      num = 15.8,
+      flag = true,
+      txt = 'true';
+      console.log(str,typeof (str));
+      console.log(sum,typeof (sum));
+      console.log(num,typeof (num));
+      console.log(flag,typeof (flag));
+      console.log(txt,typeof (txt));
+
+
+
+//2
+
+let a1 = 3 % 5,
+    a2 = 5 + '3',
+    a3 = '5' - 3,
+    a4 = 75 + 'кг',
+    a5 = 785 * 653,
+    a6 = 100 / 25,
+    a7 = 0 * 0,
+    a8 = 0 / 2,
+    a9 = 89 / 0,
+    a10 = 98 + 2,
+    a11 = 5 - 98,
+    a12 = (8 + 56 * 4) / 5,
+    a13 = (9 - 12) * 7 / (5 + 2),
+    a14 = +"123",
+    a15 = 1 || 0,
+    a16 = false || true,
+    a17 = true > 0;
+    console.log (a1, typeof a1);
+    console.log (a2, typeof a2);
+    console.log (a3, typeof a3);
+    console.log (a4, typeof a4);
+    console.log (a5, typeof a5);
+    console.log (a6, typeof a6);
+    console.log (a7, typeof a7);
+    console.log (a8, typeof a8);
+    console.log (a9, typeof a9);
+    console.log (a10,typeof a10);
+    console.log (a11, typeof a11);
+    console.log (a12, typeof a12);
+    console.log (a13, typeof a13);
+    console.log (a14, typeof a14);
+    console.log (a15, typeof a15);
+    console.log (a16, typeof a16);
+    console.log (a17, typeof a17);
+
+  //3
+   let h=10;
+   let Vcilindra = a7*h;
+   console.log (Vcilindra);      
+
+  //4
+    const p = 3.14;
+    let r = 5;
+    console.log (p*r**2);
+
+  //5
+      a = 5,
+      b = 7;
+      STrap = h * (a+b) / 2;
+      console.log (STrap);
+
+  //6
+
+      a = 8;
+      b = 3;
+
+    let x = (16-a)/2+b;
+    console.log (x);
+
+    x = (a-15)*b/(b-6);
+    console.log (x);
+
+    x = 23780/(3+a+b);
+    console.log (x);
+
+
+   //7
+
+let str1 = `индо земля зашаталась под ногами-и вырос,`,
+str2 = `и заревел он голосом диким...`,
+str3 = `блеснула молния и ударил гром,`,
+str4 = `а так какое-то чудище, страшное и мохнатое,`,
+str5 = `как будто из-под земли, перед купцом:`,
+str6 = `Он подошёл и сорвал аленький цветочек.`,
+str7 = `зверь не зверь, человек не человек,`,
+str8 = `В ту же минуту, безо всяких туч,`,
+text = str6 + str8 + str3 + str1 + str5 + str7 +str4 + str2;
+console.log(text);
